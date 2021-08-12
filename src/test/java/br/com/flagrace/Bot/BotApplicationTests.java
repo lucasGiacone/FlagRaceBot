7479be89ca3fc9d7646061ac7632c831807020eb
lucasGiacone <lucasgiacone@usp.br>
2021-08-11 23:28:01 -0300

    First commit

---
+++ src/test/java/br/com/flagrace/Bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flagrace.Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
